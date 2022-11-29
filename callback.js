4068954a91cb3aead70d28a7c5929565312d78a6
Alberes Silva <alberessilva@yahoo.com.br>
2022-11-29 14:48:41 -0300

    Explicação sobre callback

---
+++ callback.js
@@ -0,0 +1,16 @@
+/* Função de Callback */
+
+function hello(name) {
+    console.log(name)
+}
+
+
+function helloUser(callback) {
+    callback('Marcelo')
+}
+
+helloUser(hello)
+
+// const callback = e => alert('Foi clicado')
+
+// window.addEventListener('click', callback)
